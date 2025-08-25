63fdfe6390b4f527a143dae04669cb528c1355fb
burlin_gauna <burlin_gauna@yahoo.com>
2025-08-25 18:03:14 +0800

    initial setup for drone pawn and state tree

--- ScrapHaven/Source/ScrapHaven/Utilities/HavenEnums.h
+++ ScrapHaven/Source/ScrapHaven/Utilities/HavenEnums.h
@@ -10,4 +10,14 @@ enum class EOccupationType : uint8 {
 	Construction = 1	UMETA(DisplayName = "Construction"),
 	Miner = 2			UMETA(DisplayName = "Miner"),
 };
-	
+
+
+UENUM(BlueprintType)
+enum class EDroneTask : uint8 {
+	None = 0			UMETA(DisplayName = "None"),
+	Cleaning = 1	UMETA(DisplayName = "Cleaning"),
+	Supplying = 2			UMETA(DisplayName = "Supplying"),
+	Farming = 3			UMETA(DisplayName = "Farming"),
+	Delivery = 4			UMETA(DisplayName = "Delivery"),
+	Scavenging = 5			UMETA(DisplayName = "Scavenging"),
+};
